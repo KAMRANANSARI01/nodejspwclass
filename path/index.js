fb61f8755b7181a4cb37dcc42a5dc16926df783a
Kamran <123491415+KAMRANANSARI01@users.noreply.github.com>
2023-10-14 23:15:37 +0530

    added path

--- path/index.js
+++ path/index.js
@@ -2,3 +2,8 @@ const path = require("path")
 console.log(path.sep)
 console.log(process.env.PATH)
 console.log(path.delimiter)
+let join=path.format({
+    base : "index/home",
+    end: "page.html"
+})
+console.log(join)
